cf30fa7e22058ae1487ab6ba67395978fd5b016e
rustas <robert.n.o.kallstrom@gmail.com>
2022-10-12 15:08:04 +0200

    Added imports

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#Settings
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Robs_Vision's API v4" }
+
+@app.get("/posts")
+def get_posts():
+    return {"myData": "all my data"}
